c23fed51bb6389296b6fdc03170d94a57acf928e
Tralius-Mac <iustin.curcean@gmail.com>
2023-06-13 15:38:03 +0200

    cnn first steps

--- cnn_model.py
+++ cnn_model.py
@@ -1,11 +1,30 @@
 import keras
+from keras.layers import Conv1D, Dense, Flatten, Input, MaxPooling1D, SpatialDropout1D
+
 
 class CNN_MLRG:
 
     def CNN_MLRG(self):
         pass
 
-    def build_model():
+    def build_model(self):
         model = keras.Sequential()
+
+        model.add(Conv1D(32, kernel_size=3, activation='relu', input_shape=(4, 64)))
+        model.add(MaxPooling1D(pool_size=2))
+
+        # model.add(Conv1D(32, kernel_size=3, padding='same', activation='relu'))
+        # model.add(MaxPooling1D(pool_size=2))
+
+        # model.add(Conv1D(64, kernel_size=3, padding='same', activation='relu'))
+        # model.add(MaxPooling1D(pool_size=2))
+
+        model.add(Dense(9, activation="softmax"))
+
+        model.add(Flatten())
+
+        model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=['accuracy'])
+
+        model.summary()
 
         return model
