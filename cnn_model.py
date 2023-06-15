277c74ae9c6deb1a95d611956afcc274e113265b
Iusti <iustin.curcean@yahoo.com>
2023-06-15 02:54:35 +0200

    Working on cnn

--- cnn_model.py
+++ cnn_model.py
@@ -13,11 +13,11 @@ class CNN_MLRG:
         model.add(Conv1D(32, kernel_size=3, activation='relu', input_shape=(4, 64)))
         model.add(MaxPooling1D(pool_size=2))
 
-        # model.add(Conv1D(32, kernel_size=3, padding='same', activation='relu'))
-        # model.add(MaxPooling1D(pool_size=2))
+        model.add(Conv1D(32, kernel_size=3, padding='same', activation='relu'))
+        model.add(MaxPooling1D(pool_size=2))
 
-        # model.add(Conv1D(64, kernel_size=3, padding='same', activation='relu'))
-        # model.add(MaxPooling1D(pool_size=2))
+        model.add(Conv1D(32, kernel_size=3, padding='same', activation='relu'))
+        model.add(MaxPooling1D(pool_size=2))
 
         model.add(Dense(9, activation="softmax"))
 
