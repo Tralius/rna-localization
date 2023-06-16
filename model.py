7a852c6faae6271c45eef760d65c86c398b0014b
jonas2612 <ge49zen@mytum.de>
2023-06-16 14:01:28 +0200

    finished pipeline and inproved abstract class

--- model.py
+++ model.py
@@ -1,15 +1,21 @@
 from abc import ABC, abstractmethod
 from GeneDataLoader import GeneDataLoader
+import pandas as pd
 
 class Model(ABC):
-    def __init__(self, data, **kwargs) -> None:
-        self.loader = GeneDataLoader(data_table=data, **kwargs)
-        self.model = None
+    def __init__(self, train_data: pd.DataFrame, validation_data: pd.DataFrame = None, **kwargs) -> None:
+        batch_size_train = kwargs.pop('batch_size_train')
+        shuffle_batch_train = kwargs.pop('shuffle_batch_train')
+        self.train_data_loader = GeneDataLoader(train_data, batch_size=batch_size_train, shuffle=shuffle_batch_train)
+        if validation_data is not None:
+            batch_size_valid = kwargs.pop('batch_size_valid')
+            shuffle_batch_valid = kwargs.pop('shuffle_batch_valid')
+            self.validation_data_loader = GeneDataLoader(validation_data, batch_size=batch_size_valid, shuffle=shuffle_batch_valid)
 
     @abstractmethod
-    def train(loss, **kwargs):
+    def fit(self, **kwargs):
         pass
 
     @abstractmethod
-    def predict(data, **kwargs):
+    def evaluate(self, **kwargs):
         pass
